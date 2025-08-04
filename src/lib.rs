107bd4337d672e90b07a2a1a84d141959b901149
Jon Tourville <jontourville@me.com>
2025-08-04 08:49:42 -0500

    Load tasks from todo list

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,52 @@
-use std::env;
+use std::fs;
+
+pub struct Task {
+    pub name: String,
+    pub order: i32,
+}
 
 pub fn parse_command(
     mut args: impl Iterator<Item = String>,
 ) -> Result<String, &'static str> {
-    args.next();
+    let list_path = String::from(".todo");
 
+    args.next();
     let command = match args.next() {
         Some(arg) => arg,
         None => String::from("list"),
     };
 
     match command.as_str() {
-        "list" => println!("Listing tasks..."),
+        "list" => list_tasks(&list_path),
         "add" => println!("Adding task..."),
         "remove" => println!("Removing task..."),
         "move" => println!("Moving task..."),
         _ => return Err("unrecognized command"),
     }
 
-    //JMT TODO: change to list_path
-    Ok(String::from(".todo"))
+    Ok(list_path)
+}
+
+fn load_tasks(list_path: &String) -> Vec<String> {
+    fs::read_to_string(list_path)
+        .unwrap_or_default()
+        .lines()
+        .map(String::from)
+        .collect()
+}
+
+fn list_tasks(list_path: &String) {
+    let mut i = 1;
+    let tasks = load_tasks(list_path);
+
+    println!("TODO List:");
+
+    if tasks.len() == 0 {
+        println!("    (empty)");
+    }
+
+    for task in tasks.iter() {
+        println!("{:5}. {}", i, task);
+        i = i + 1;
+    }
 }
